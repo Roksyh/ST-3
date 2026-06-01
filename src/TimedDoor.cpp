bcc12417109e9658e4ff33768d36686e0817781d
Rjomb ER <loskutnikov_2005@mail.ru>
2026-06-01 14:50:13 +0300

    b170-3

--- src/TimedDoor.cpp
+++ src/TimedDoor.cpp
@@ -1,2 +1,57 @@
 // Copyright 2021 GHA Test Team
+
 #include "TimedDoor.h"
+
+#include <chrono>
+#include <stdexcept>
+#include <thread>
+
+DoorTimerAdapter::DoorTimerAdapter(TimedDoor& d) : door(d) {}
+
+void DoorTimerAdapter::Timeout() {
+  door.throwState();
+}
+
+TimedDoor::TimedDoor(int timeout) : adapter(nullptr),
+                                    iTimeout(timeout),
+                                    isOpened(false) {
+  adapter = new DoorTimerAdapter(*this);
+}
+
+TimedDoor::~TimedDoor() {
+  delete adapter;
+}
+
+bool TimedDoor::isDoorOpened() {
+  return isOpened;
+}
+
+void TimedDoor::unlock() {
+  isOpened = true;
+  Timer timer;
+  timer.tregister(iTimeout, adapter);
+}
+
+void TimedDoor::lock() {
+  isOpened = false;
+}
+
+int TimedDoor::getTimeOut() const {
+  return iTimeout;
+}
+
+void TimedDoor::throwState() {
+  if (isOpened) {
+    throw std::runtime_error("Door remained open past timeout");
+  }
+}
+
+void Timer::sleep(int seconds) {
+  std::this_thread::sleep_for(std::chrono::seconds(seconds));
+}
+
+void Timer::tregister(int timeout, TimerClient* c) {
+  client = c;
+  sleep(timeout);
+  client->Timeout();
+}
--- test/tests.cpp
+++ test/tests.cpp
@@ -2,5 +2,110 @@
 
 #include <gtest/gtest.h>
 #include <gmock/gmock.h>
-#include <cstdint>
+#include <thread>
+#include <chrono>
+#include <stdexcept>
 #include "TimedDoor.h"
+
+class MockTimerClient : public TimerClient {
+ public:
+  MOCK_METHOD(void, Timeout, (), (override));
+};
+
+class MockDoor : public Door {
+ public:
+  MOCK_METHOD(void, lock, (), (override));
+  MOCK_METHOD(void, unlock, (), (override));
+  MOCK_METHOD(bool, isDoorOpened, (), (override));
+};
+
+class TimedDoorTest : public ::testing::Test {
+ protected:
+  TimedDoor* door;
+
+  void SetUp() override {
+    door = new TimedDoor(0);
+  }
+
+  void TearDown() override {
+    delete door;
+  }
+};
+
+TEST_F(TimedDoorTest, GetTimeoutReturnsCorrectValue) {
+  EXPECT_EQ(door->getTimeOut(), 0);
+}
+
+TEST_F(TimedDoorTest, DoorInitiallyClosed) {
+  EXPECT_FALSE(door->isDoorOpened());
+}
+
+TEST_F(TimedDoorTest, LockClosesDoor) {
+  door->lock();
+  EXPECT_FALSE(door->isDoorOpened());
+}
+
+TEST_F(TimedDoorTest, ThrowStateNoThrowWhenClosed) {
+  EXPECT_NO_THROW(door->throwState());
+}
+
+TEST_F(TimedDoorTest, ThrowStateThrowsWhenOpen) {
+  try { door->unlock(); } catch (const std::runtime_error&) {}
+  EXPECT_THROW(door->throwState(), std::runtime_error);
+}
+
+TEST_F(TimedDoorTest, UnlockThrowsIfDoorStaysOpen) {
+  EXPECT_THROW(door->unlock(), std::runtime_error);
+}
+
+TEST_F(TimedDoorTest, DoorIsOpenAfterUnlockThrows) {
+  EXPECT_THROW(door->unlock(), std::runtime_error);
+  EXPECT_TRUE(door->isDoorOpened());
+}
+
+TEST_F(TimedDoorTest, AdapterTimeoutThrowsWhenOpen) {
+  try { door->unlock(); } catch (const std::runtime_error&) {}
+  DoorTimerAdapter adapter(*door);
+  EXPECT_THROW(adapter.Timeout(), std::runtime_error);
+}
+
+TEST_F(TimedDoorTest, AdapterTimeoutNoThrowWhenClosed) {
+  DoorTimerAdapter adapter(*door);
+  EXPECT_NO_THROW(adapter.Timeout());
+}
+
+TEST_F(TimedDoorTest, TimerCallsClientTimeout) {
+  MockTimerClient mockClient;
+  EXPECT_CALL(mockClient, Timeout()).Times(1);
+  Timer timer;
+  timer.tregister(0, &mockClient);
+}
+
+TEST_F(TimedDoorTest, CloseBeforeTimeoutPreventsException) {
+  TimedDoor timedDoor(1);
+  std::thread closer([&timedDoor]() {
+    std::this_thread::sleep_for(std::chrono::milliseconds(100));
+    timedDoor.lock();
+  });
+  EXPECT_NO_THROW(timedDoor.unlock());
+  closer.join();
+}
+
+TEST(MockDoorTest, LockIsCalled) {
+  MockDoor mockDoor;
+  EXPECT_CALL(mockDoor, lock()).Times(1);
+  mockDoor.lock();
+}
+
+TEST(MockDoorTest, UnlockIsCalled) {
+  MockDoor mockDoor;
+  EXPECT_CALL(mockDoor, unlock()).Times(1);
+  mockDoor.unlock();
+}
+
+TEST(MockDoorTest, IsOpenedReturnsValue) {
+  MockDoor mockDoor;
+  EXPECT_CALL(mockDoor, isDoorOpened())
+      .WillOnce(::testing::Return(true));
+  EXPECT_TRUE(mockDoor.isDoorOpened());
+}
